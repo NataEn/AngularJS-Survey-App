e7b5bbaed74a59c9a8398757fed080a9bdde3055
Natalie Enkin <natalie@enkin.co.il>
2021-06-12 11:49:08 +0300

    updating survey question view and next an prev functions

--- app.js
+++ app.js
@@ -56,18 +56,37 @@ function SurveysController($scope) {
       ],
     },
   ];
-  $scope.currentSurvey = $scope.surveys[1];
-  $scope.currentSurveyQuestion = $scope.currentSurvey.questions[0];
-  $scope.setCurrentSurvey = function (num) {
+  $scope.currentSurveyIndex = 0;
+  $scope.currentSurveyQuestionIndex = 0;
+  $scope.setCurrentSurveyIndex = function (setNum) {
     $scope.currentSurvey[num];
   };
-  $scope.setCurrentQuestion = function (num) {
-    const questionsLength = $scope.currentSurvey.questions.length;
-    const nextQuestionIndex = questionsLength + num;
-    if (nextQuestionIndex > 0 || nextQuestionIndex < questionsLength) {
-      $scope.currentSurveyQuestion =
-        $scope.currentSurvey.questions[nextQuestionIndex];
-    } else $scope.currentSurveyQuestion = $scope.currentSurvey.questions[0];
+  $scope.incrementCurrentQuestionIndex = function (setNum) {
+    const num = Number(setNum);
+
+    const questionsLength =
+      $scope.surveys[$scope.currentSurveyIndex].questions.length;
+    const nextQuestionIndex = $scope.currentSurveyQuestionIndex + num;
+
+    if (num > 0 && nextQuestionIndex < questionsLength - 1) {
+      $scope.currentSurveyQuestionIndex = nextQuestionIndex;
+    } else {
+      $scope.currentSurveyQuestionIndex = 0;
+    }
+  };
+
+  $scope.decrementCurrentQuestionIndex = function (setNum) {
+    const num = Number(setNum);
+
+    const questionsLength =
+      $scope.surveys[$scope.currentSurveyIndex].questions.length;
+    const nextQuestionIndex = $scope.currentSurveyQuestionIndex + num;
+
+    if (num < 0 && nextQuestionIndex >= 0) {
+      $scope.currentSurveyQuestionIndex = nextQuestionIndex;
+    } else {
+      $scope.currentSurveyQuestionIndex = questionsLength - 1;
+    }
   };
 }
 app.controller("SurveysController", SurveysController);
